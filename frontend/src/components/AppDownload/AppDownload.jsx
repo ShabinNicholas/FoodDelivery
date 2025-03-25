db6b415c48acbb3632e23ec268cd3864250dacc7
Shabin7 <shabin.nj@gmail.com>
2025-03-25 10:06:26 +0530

    created some more components for the products

---
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './AppDownload.css'
+
+const AppDownload = () => {
+  return (
+    <div className='app-download'>AppDownload</div>
+  )
+}
+
+export default AppDownload
