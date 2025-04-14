e8e7af7e07ae0bd6237002f456b6da535bfe69e8
Shabin7 <shabin.nj@gmail.com>
2025-04-14 23:04:16 +0530

    user creation integreated in the frontend and backend

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,10 +1,9 @@
 import React, { useEffect, useState } from "react";
 import "./List.css";
 import axios from "axios";
-import { url } from "../../assets/assets";
 import { toast } from "react-toastify";
 
-const List = () => {
+const List = ({url}) => {
   const [list, setList] = useState([]);
   const fetchList = async () => {
     const response = await axios.get(`${url}/api/food/list`);
